586f80d5b629c7b08288c93e8aa488d0a66a71ba
saifmathur <saifmathur41@gmail.com>
2020-06-07 11:24:39 +0530

    added static data, installed pug extension

--- app.js
+++ app.js
@@ -8,9 +8,41 @@ app.set('view engine','pug');
 
 //home route
 app.get('/', function (req, res){
+
+    let articles = [
+        {
+            id: 1,
+            title: 'article one',
+            author: 'saif',
+            body: 'this is article one'
+        },
+        {
+            id: 2,
+            title: 'article two',
+            author: 'saif',
+            body: 'this is article two'
+        },
+        {
+            id: 3,
+            title: 'article three',
+            author: 'saif',
+            body: 'this is article three'
+        },
+        {
+            id: 4,
+            title: 'article four',
+            author: 'saif',
+            body: 'this is article four'
+        },
+
+
+
+
+    ]
+
     res.render('index', {
-        title: 'Home',
-        name: 'saif'
+        title: 'Articles',
+        articles: articles
     });
 });
 
